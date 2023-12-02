e3b33d9ac0bcace624f1c03d6b994d8cfd74f676
RodrigoAnciaes <81170574+RodrigoAnciaes@users.noreply.github.com>
2023-12-02 17:51:23 -0300

    revertendo.

--- bucket/bucket.tf
+++ bucket/bucket.tf
@@ -1,4 +1,20 @@
 # Create an AWS S3 bucket resource named "bucket_terraform"
 resource "aws_s3_bucket" "bucket_terraform" {
-  bucket = "ro-testepatelli-terraform-state" # The name of the bucket
+  bucket = "rodrigopatelli-terraform-state" # The name of the bucket
 }
+
+# # Enable versioning for the S3 bucket
+# resource "aws_s3_bucket_versioning" "versioning" {
+#   bucket = aws_s3_bucket.bucket_terraform.id # The name of the bucket
+
+#   versioning_configuration {
+#     status = "Enabled" # Enable versioning
+#   }
+# }
+
+# # Create an S3 object in the bucket named "terraform.tfstate"
+# resource "aws_s3_object" "s3_object" {
+#   bucket        = aws_s3_bucket.bucket_terraform.id # The name of the bucket
+#   key           = "terraform.tfstate" # The name of the object
+#   force_destroy = true # Allow for forceful deletion of the object
+# }
